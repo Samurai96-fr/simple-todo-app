f482566e0124b501ace2829b60fd86c4100f837c
Cédric Reboux <cedreboux@gmail.com>
2020-06-18 10:51:59 +0200

    Updates

---
+++ precache-manifest.ede8018bd1def4bfada2986bc85382ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65a3b94a15ae3f8e0c2177bf0a5ae8e",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "e3b28d7ed5c93591dd7a",
+    "url": "/simple-todo-app/static/css/main.d9012f66.chunk.css"
+  },
+  {
+    "revision": "0139ce81d585db9a214e",
+    "url": "/simple-todo-app/static/js/2.b9476618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.b9476618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b28d7ed5c93591dd7a",
+    "url": "/simple-todo-app/static/js/main.a80ef9ce.chunk.js"
+  },
+  {
+    "revision": "e165d461a30ce2094b0e",
+    "url": "/simple-todo-app/static/js/runtime-main.065a362d.js"
+  }
+]);
